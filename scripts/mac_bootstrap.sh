2e6d04a86d90bedbe20ebc89ab899d1827337d84
Steve Luppi <steve@rubiconmd.com>
2023-11-30 12:48:15 -0600

    using my own file

--- scripts/mac_bootstrap.sh
+++ scripts/mac_bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Run with: /bin/bash -c "$(curl https://raw.githubusercontent.com/iovis/dotfiles/master/scripts/mac_bootstrap.sh)"
+# Run with: /bin/bash -c "$(curl https://raw.githubusercontent.com/steveluppi/dotfiles/main/scripts/mac_bootstrap.sh)"
 
 # Dotfiles
 echo "[$(date '+%Y-%m-%d %H:%M')] Checking Dotfiles"
 if [ ! -d ~/.dotfiles ]; then
 	echo "[$(date '+%Y-%m-%d %H:%M')] Cloning Dotfiles"
-	git clone https://github.com/iovis/dotfiles ~/.dotfiles
+	git clone https://github.com/steveluppi/dotfiles ~/.dotfiles
 else
 	echo "[$(date '+%Y-%m-%d %H:%M')] Getting Latest Dotfiles"
 	cd ~/.dotfiles; git pull; cd ~
