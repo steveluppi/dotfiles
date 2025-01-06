e5c25075275273730bc9a683239001504f663f8e
Steve Luppi <steve@rubiconmd.com>
2025-01-06 12:22:47 -0600

    Updates to hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -31,6 +31,7 @@ local tmux_send_it = function(to_send)
 end
 
 hs.hotkey.bind(MY_MODS, 'pad.', function() hs.alert.show('Main') end)
+hs.hotkey.bind(MY_MODS, '.', function() hs.alert.show('Main') end)
 
 -- Easy way to reload while working on the config
 hs.hotkey.bind(MY_MODS, "r", function() hs.reload() end)
@@ -39,25 +40,31 @@ modal_shorts = hs.hotkey.modal.new(MY_MODS, 'pad0')
 function modal_shorts:entered() hs.alert'Entered Shortcuts Mode' end
 function modal_shorts:exited() hs.alert'Exited Shortcuts Mode' end
 modal_shorts:bind(MY_MODS, 'pad.', function() hs.alert.show('Shortcuts') end)
+modal_shorts:bind(MY_MODS, '.', function() hs.alert.show('Shortcuts') end)
 modal_shorts:bind(MY_MODS, 'pad*', function() modal_shorts:exit() end)
 
 modal_meet = hs.hotkey.modal.new(MY_MODS, 'pad5')
 function modal_meet:entered() hs.alert'Entered Meeting Mode' end
 function modal_meet:exited() hs.alert'Exited Meeting Mode' end
 modal_meet:bind(MY_MODS, 'pad.', function() hs.alert.show('Meeting') end)
+modal_meet:bind(MY_MODS, '.', function() hs.alert.show('Meeting') end)
 modal_meet:bind(MY_MODS, 'pad*', function() modal_meet:exit() end)
 modal_meet:bind('', "pad0", function() hs.eventtap.keyStroke('alt', 'y') end)
 modal_meet:bind('', "pad1", function() hs.eventtap.keyStroke('cmd-shift', 'a') end)
 modal_meet:bind('', "pad2", function() hs.eventtap.keyStroke('cmd-shift', 'v') end)
 modal_meet:bind('', "pad3", function() hs.eventtap.keyStroke('cmd-shift', 'd') end)
+modal_meet:bind('', "pad4", function() hs.eventtap.keyStroke('cmd', 'd') end)
 modal_meet:bind('', "pad5", function() hs.eventtap.keyStroke('cmd-shift', 'f') end)
+modal_meet:bind('', "pad6", function() hs.eventtap.keyStroke('cmd', 'e') end)
 modal_meet:bind('', "pad7", function() hs.shortcuts.run("Desk Audio") end)
 modal_meet:bind('', "pad9", function() hs.shortcuts.run("Headset Audio") end)
+modal_meet:bind('', 'pad+', function() hs.eventtap.keyStroke('cmd-shift', '5') end)
 
 modal_dev = hs.hotkey.modal.new(MY_MODS, 'pad2')
 function modal_dev:entered() hs.alert'Entered Dev Mode' end
 function modal_dev:exited() hs.alert'Exited Dev Mode' end
 modal_dev:bind(MY_MODS, 'pad.', function() hs.alert.show('Dev') end)
+modal_dev:bind(MY_MODS, '.', function() hs.alert.show('Dev') end)
 modal_dev:bind(MY_MODS, 'pad*', function() modal_dev:exit() end)
 modal_dev:bind('', 'pad0', tmux_send_it('0'))
 modal_dev:bind('', 'pad1', tmux_send_it('1'))
@@ -70,9 +77,11 @@ modal_test = hs.hotkey.modal.new(MY_MODS, 'pad3')
 function modal_test:entered() hs.alert'Entered Test Mode' end
 function modal_test:exited() hs.alert'Exited Test Mode' end
 modal_test:bind(MY_MODS,'pad.', function() hs.alert.show('Test') end)
+modal_test:bind(MY_MODS,'.', function() hs.alert.show('Test') end)
 modal_test:bind(MY_MODS, 'pad*', function() modal_test:exit() end)
 
--- Work, personal zoom link from a hotkey!
+-- Work, email and personal zoom link from a hotkey!
+hs.hotkey.bind(MY_MODS, "e", send_it("steve@rubiconmd.com"))
 hs.hotkey.bind(MY_MODS, "z", send_it("https://rubiconmd.zoom.us/j/7111411118"))
 
 local f = io.open('./priv/init.lua', 'r')
@@ -90,6 +99,22 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, 'i', function()
   hs.eventtap.keyStrokes(time)
 end)
 
+hs.hotkey.bind('', 'F13', function()
+  local win = hs.window.focusedWindow()
+  local log = hs.logger.new('windowPos','debug')
+  log.i(win)
+  log.i(win:frame())
+  win = hs.window.find('Slack')
+  log.i(win)
+end)
+hs.hotkey.bind(MY_MODS, 't', function()
+  local win = hs.window.focusedWindow()
+  win:move(hs.geometry.rect(0,1730, 1280,430))
+end)
+hs.hotkey.bind('', 'F19', function()
+  local win = hs.window.focusedWindow()
+  win:move(hs.geometry.rect(0,1730, 1280,430))
+end)
 
 open_app('', "F2", "Wezterm")
 open_app('', "F16", "Wezterm")
